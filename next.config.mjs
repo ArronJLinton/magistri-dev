9eb780cf68c5d11cb090b06f450e63fd7c2cedea
Arron Linton <lintonarron@gmail.com>
2025-03-24 00:38:11 -0400

    ./out

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export', // Ensures static export
-  images: {
-    unoptimized: true, // GitHub Pages does not support Next.js image optimization
-  },
-  basePath: '/magistri-dev', // Replace with your actual repo name
-  assetPrefix: '/magistri-dev',
-};
+const nextConfig = {};
 
 export default nextConfig;
