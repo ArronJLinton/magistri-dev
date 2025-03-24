76a8edc25798b3fa4310e5e6a93791fa1155da48
Arron Linton <lintonarron@gmail.com>
2025-03-24 00:22:55 -0400

    troubleshooting 404

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export', // Ensures static export
+  images: {
+    unoptimized: true, // GitHub Pages does not support Next.js image optimization
+  },
+  basePath: '/magistri-dev', // Replace with your actual repo name
+  assetPrefix: '/magistri-dev',
+};
 
 export default nextConfig;
