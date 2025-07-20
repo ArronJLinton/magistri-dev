c34e8a46b78ffa85e1ca6d4d25f7d79c66178b50
Arron Linton <lintonarron@gmail.com>
2025-07-20 13:34:46 -0400

    updated copy

--- src/layouts/footers/FooterOne.tsx
+++ src/layouts/footers/FooterOne.tsx
@@ -27,28 +27,6 @@ const FooterOne = ({ style_2 }: any) => {
                       className="w-1/2 sm:w-full h-[200px] object-contain"
                     />
                   </a>
-                  {/* </div> */}
-                  {/* <div className="it-footer__text pb-15">
-                    <p>
-                      There is no one who loves pain itsel <br /> seeks after it
-                      and wants to have{' '}
-                    </p>
-                  </div> */}
-                  {/* <div className="it-footer__contact-list">
-                    <ul> */}
-                  {/* <li>
-                        <a href="tel:+8973534529245">+897 35345 29245</a>
-                      </li> */}
-                  {/* <li>
-                        <a
-                          className={`color ${style_2 ? 'color-red' : ''}`}
-                          href="mailto:www.tecch.rk-theme.com"
-                        >
-                          www.tecch.rk-theme.com`
-                        </a>
-                      </li> */}
-                  {/* </ul> */}
-                  {/* </div> */}
                 </div>
               </div>
 
@@ -69,10 +47,6 @@ const FooterOne = ({ style_2 }: any) => {
                         <i className="flaticon-next"></i>
                         <a href="/#services">Our Services</a>
                       </li>
-                      {/* <li>
-                        <i className="flaticon-next"></i>
-                        <a href="#">News & Media</a>
-                      </li> */}
                       <li>
                         <i className="flaticon-next"></i>
                         <a href="/#projects">Our Projects</a>
@@ -85,90 +59,6 @@ const FooterOne = ({ style_2 }: any) => {
                   </div>
                 </div>
               </div>
-              {/* Blog */}
-              {/* <div
-                className="col-xl-4 col-lg-4 col-md-6 mb-30 wow itfadeUp"
-                data-wow-duration=".9s"
-                data-wow-delay=".7s"
-              >
-                <div className="it-footer__widget footer-col-3">
-                  <h4 className="it-footer__widget-title">Recent Blogs</h4>
-                  <div className="it-footer__gallery-box">
-                    <div className="it-footer__gallery d-flex align-items-center">
-                      <img
-                        src="assets/img/footer/footer-sm-1-1.jpg"
-                        alt="rk-theme"
-                      />
-                      <div className="it-footer__gallery-content">
-                        <span className={`${style_2 ? 'color-red' : ''}`}>
-                          <i className="far fa-clock"></i>02 Feb, 2023
-                        </span>
-                        <p>
-                          Cras quis tempus diam. In vel orci id sem pretium comm
-                        </p>
-                      </div>
-                    </div>
-                    <div className="it-footer__gallery d-flex align-items-center">
-                      <img
-                        src="assets/img/footer/footer-sm-1-2.jpg"
-                        alt="rk-theme"
-                      />
-                      <div className="it-footer__gallery-content">
-                        <span className={`${style_2 ? 'color-red' : ''}`}>
-                          <i className="far fa-clock"></i>02 Feb, 2023
-                        </span>
-                        <p>
-                          There is no one who loves pai itsel who seeks after it
-                          and
-                        </p>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div> */}
-
-              {/* Subscribe */}
-              {/* <div
-                className="col-xl-3 col-lg-4 col-md-6 mb-30 wow itfadeUp"
-                data-wow-duration=".9s"
-                data-wow-delay=".9s"
-              >
-                <div className="it-footer__widget footer-col-4">
-                  <h4 className="it-footer__widget-title">Subscribe</h4>
-                  <div className="it-footer__input-box">
-                    <input type="text" placeholder="Keep in touch" />
-                    <div
-                      className={`it-footer__button-box ${
-                        style_2 ? 'color-red' : ''
-                      }`}
-                    >
-                      <button>
-                        <i className="fas fa-arrow-right"></i>
-                      </button>
-                    </div>
-                  </div>
-                  <div className="it-footer__text pb-10">
-                    <p>
-                      Our expertise, as well as our passion for web <br />
-                      design, sets us agenciese
-                    </p>
-                  </div>
-                  <div className="it-footer__social">
-                    <a href="#">
-                      <i className="fa-brands fa-facebook-f"></i>
-                    </a>{' '}
-                    <a href="#">
-                      <i className="fa-brands fa-instagram"></i>
-                    </a>{' '}
-                    <a href="#">
-                      <i className="fa-brands fa-linkedin-in"></i>
-                    </a>{' '}
-                    <a href="#">
-                      <i className="fa-brands fa-twitter"></i>
-                    </a>{' '}
-                  </div>
-                </div>
-              </div> */}
             </div>
           </div>
         </div>
